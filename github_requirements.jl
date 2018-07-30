7da5236206fec87aa997b6d6e6d1130a10de2f80
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-07-30 20:14:00 +0200

    Add QI dependency

---
+++ github_requirements.jl
@@ -0,0 +1,10 @@
+#=
+github_requirements:
+- Julia version: 
+- Author: marcin
+- Date: 2018-07-30
+=#
+
+if !("QI" in keys(Pkg.installed()))
+    Pkg.clone("https://github.com/mprzewie/QI.jl")
+end
