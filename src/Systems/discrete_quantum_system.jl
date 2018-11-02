7d2fee59f5eaa500cc504b2d6568280e5032592b
Marcin Przewięźlikowski <m.przewie@gmail.com>
2018-11-02 03:23:32 +0100

    Prepend_parent no longer needed

--- src/Systems/discrete_quantum_system.jl
+++ src/Systems/discrete_quantum_system.jl
@@ -21,16 +21,6 @@ function check_distribution(
     dimensions[1] == dimensions[2] == total_ncategories
 end
 
-function prepend_parent(dqs::DiscreteQuantumSystem, var::Variable)
-    if !(var in parents(dqs))
-        new_parents = vcat([var], parents(dqs))
-        new_distribution = kron(eye(var.ncategories), dqs.distribution)
-        return DiscreteQuantumSystem(new_parents, variables(dqs), new_distribution)
-    else
-        return dqs
-    end
-end
-
 permute_distribution(d::QuantumDistribution, dimensions::Vector{Int64}, order::Vector{Int64}) = permute_systems(d, dimensions, order)
 
 function reduce_distribution(d::QuantumDistribution, dimensions::Vector{Int64}, reduce_ind::Vector{Int64})
